f85e868e30a601df5ad7fc076a8bbbe41378453a
maiko-m <mn.6182.nm@gmail.com>
2020-05-24 18:21:14 +0900

    init

---
+++ multiple-select-table/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  multiple-select-table
+//
+//  Created by maiko morisaki on 2020/05/24.
+//  Copyright © 2020 nyapple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
